5b7ec6dbe2bc5f5c358bcbf4f9bf546e62a17e2b
313 Team - Islamic Cyber ​​Resistance in Iraq <op313team@protonmail.com>
2025-07-25 07:44:49 -0700

    Add files via upload

---
+++ src/313AlmuntaqimVirus/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("313 AlmuntaqimVirus")]
+[assembly: AssemblyDescription("313 Team")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("313 Team")]
+[assembly: AssemblyProduct("313 AlmuntaqimVirus")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("Hacker Al-Imamah313")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a43f94d-e5e9-41d3-9f28-6af3a86c918b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
